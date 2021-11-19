928026f448e2aeff417e496312f678bddb31fe5d
Muhammed Ademola <muhammeda@usf.edu>
2021-11-18 23:12:22 -0500

    Update

--- StockDataForm.cs
+++ StockDataForm.cs
@@ -15,6 +15,8 @@ namespace StockMarketProject
         public StockDataForm()
         {
             InitializeComponent();
+            //string url = "https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1605639981&period2=1637175981&interval=1d&events=history&includeAdjustedClose=true";
+            //string output = webClient.DownloadString(url);
         }
 
         public StockDataForm(string stockName, string start, string end, string interval)
@@ -22,9 +24,9 @@ namespace StockMarketProject
             //start and end time must be in unix epoch time
             string URL = "https://query1.finance.yahoo.com/v7/finance/download/" + stockName + "?period1=" + start + "&period2=" + end + "&interval=" + interval + "&events=history&includeAdjustedClose=true";
             System.Net.WebClient webClient = new System.Net.WebClient();
-            string url = "https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1605639981&period2=1637175981&interval=1d&events=history&includeAdjustedClose=true";
+            
             string output = webClient.DownloadString(URL);
-            //string output = webClient.DownloadString(url);
+            
             string[] DataRows = output.Split('\n');
             foreach (string line in DataRows)
             {
